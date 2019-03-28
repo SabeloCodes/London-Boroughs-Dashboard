41d50e42eff20476d17322a0d7db3b183ef6fbdc
Sabelo Mcanyana <sabelom@gmail.com>
2019-03-28 16:30:46 +0000

    Changed page layout using flexbox

--- mileStoneProject/static/js/index.js
+++ mileStoneProject/static/js/index.js
@@ -28,39 +28,39 @@ function makeGraphs(error, housingData) {
 
 // Chart emmissions Per Borough:
 function show_emissions_per_borough(ndx) {
-        var borough_dim = ndx.dimension(dc.pluck('borough'));
-        var borough_emissions_group = borough_dim.group().reduceSum(dc.pluck('carbonEmissions'));
-        dc.barChart('#emissions-borough')
-            .width(800)
-            .height(250)
-            .margins({top: 10, right: 50, bottom: 30, left: 50})
-            .dimension(borough_dim)
-            .group(borough_emissions_group)
-            .transitionDuration(600)
-            .x(d3.scale.ordinal())
-            .xUnits(dc.units.ordinal)
-            .xAxisLabel("Borough")
-            .yAxisLabel("Emissions")
-            .yAxis().ticks(40);
+    var borough_dim = ndx.dimension(dc.pluck('borough'));
+    var borough_emissions_group = borough_dim.group().reduceSum(dc.pluck('carbonEmissions'));
+    dc.barChart('#emissions-borough')
+        .width(600)
+        .height(350)
+        .margins({top: 10, right: 50, bottom: 30, left: 50})
+        .dimension(borough_dim)
+        .group(borough_emissions_group)
+        .transitionDuration(600)
+        .x(d3.scale.ordinal())
+        .xUnits(dc.units.ordinal)
+        .xAxisLabel("Borough")
+        .yAxisLabel("Emissions")
+        .yAxis().ticks(40);
 }
 
 
 // Chart Cars Per Borough:
 function show_cars_per_borough(ndx) {
-        var borough_dim = ndx.dimension(dc.pluck('borough'));
-        var borough_cars_group = borough_dim.group().reduceSum(dc.pluck('cars'));
-        dc.barChart('#cars-borough')
-            .width(800)
-            .height(250)
-            .margins({top: 10, right: 50, bottom: 30, left: 50})
-            .dimension(borough_dim)
-            .group(borough_cars_group)
-            .transitionDuration(600)
-            .x(d3.scale.ordinal())
-            .xUnits(dc.units.ordinal)
-            .xAxisLabel("Borough")
-            .yAxisLabel("Cars")
-            .yAxis().ticks(40);
+    var borough_dim = ndx.dimension(dc.pluck('borough'));
+    var borough_cars_group = borough_dim.group().reduceSum(dc.pluck('cars'));
+    dc.barChart('#cars-borough')
+        .width(600)
+        .height(350)
+        .margins({top: 10, right: 50, bottom: 30, left: 50})
+        .dimension(borough_dim)
+        .group(borough_cars_group)
+        .transitionDuration(600)
+        .x(d3.scale.ordinal())
+        .xUnits(dc.units.ordinal)
+        .xAxisLabel("Borough")
+        .yAxisLabel("Cars")
+        .yAxis().ticks(40);
 }
 
 
@@ -128,6 +128,8 @@ function show_population_emissions_correlation(ndx) {
     });
 
     var emissionsPollutionGroup = pollution_dim.group();
+    
+    console.log(emissionsPollutionGroup.all());
 
     var minEmissions = emissions_dim.bottom(1)[0].carbonEmissions;
     var maxEmissions = emissions_dim.top(1)[0].carbonEmissions;
@@ -135,11 +137,13 @@ function show_population_emissions_correlation(ndx) {
         dc.scatterPlot("#population-emissions")
         .width(1000)
         .height(400)
+        .margins({top: 10, right: 50, bottom: 75, left: 75})
         .x(d3.scale.linear().domain([minEmissions, maxEmissions]))
         .brushOn(false)
+        .symbol("square")
         .symbolSize(8)
         .clipPadding(10)
-        .elasticY(false)
+        .elasticY(true)
         .yAxisLabel("Population")
         .xAxisLabel("Emissions")
         .title(function(d) {
@@ -147,7 +151,7 @@ function show_population_emissions_correlation(ndx) {
         })
         .dimension(pollution_dim)
         .group(emissionsPollutionGroup)
-        .margins({top: 10, right: 50, bottom: 75, left: 75});
+        
 }
 
 
